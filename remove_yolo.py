7c6ff811c44c9981cdf9c473389fa7ec5125470a
Hoa <huynhlequang@gmail.com>
2021-05-12 10:22:56 +0900

    Update script remove_yolo.py

--- remove_yolo.py
+++ remove_yolo.py
@@ -14,27 +14,27 @@ if __name__ == "__main__":
     imgDir = args.img  + '/'
     annDir = args.ann
     anns = glob(annDir + '/*.txt')
-    exts = ['.jpg', '.png', '.jpeg', '.jfif', '.JPG']
+    exts = ['.jpg', '.png', '.jpeg', '.jfif']
     imgs = list()
     for ext in exts:
         imgs += glob(imgDir + '*' + ext)
     for img in imgs:
-        annpath = annDir + '/' + os.path.basename(img).split('.')[0] + ext
-        if os.path.exists(annpath) is True:
-            print("exist: ", annpath)
-        else:
+        annpath = annDir + '/' + os.path.basename(img).split('.')[0] + '.txt'
+        # if os.path.exists(annpath) is True:
+            # print("exist: ", annpath)
+        if os.path.exists(annpath) is False:
             print("redundant: ", img)
             os.remove(img)            
     for ann in anns:
         idx = 0
         for ext in exts:
-            print(".", end="", flush=True)
-            imgpath = imgDir + os.path.basename(ann).split('.')[0] + ext
-            if os.path.exists(imgpath) is True:
-                print("exist: ", imgpath)
+            print(".", end="", flush=true)
+            imgpath = imgdir + os.path.basename(ann).split('.')[0] + ext
+            if os.path.exists(imgpath) is true:
+                #print("exist: ", imgpath)
                 break
             idx += 1
         if idx == len(exts):
             print("redundant: ", annpath)
             os.remove(ann)
-    print('Finished!')
+    print('finished!')

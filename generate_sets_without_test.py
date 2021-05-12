60641af520c69b57ff84617b53a023c705edeecd
Hoa <huynhlequang@gmail.com>
2021-05-12 10:17:41 +0900

    Add scripts to generate train.txt, valid.txt, and test.txt

---
+++ generate_sets_without_test.py
@@ -0,0 +1,38 @@
+import os
+from glob import glob
+import random
+
+data = list()
+exts = ['.jpg', '.png', '.jfif', '.jpeg']
+extLen = len(exts)
+impaths = glob('img\\*.txt')
+log = open("log_generate.txt", "rt")
+lines = list()
+for line in log:
+    lines.append(os.path.basename(line).split('.')[0])
+log.close()
+for impath in impaths:
+    basename = os.path.basename(impath).split('.')[0]
+    if basename in lines:
+        print('Not exist: ' + basename)
+        continue
+    idx = 0
+    for ext in exts:
+        fileName = 'img/' + basename + ext
+        if os.path.exists(fileName):
+            #print(fileName)
+            data.append('data/' + fileName + '\n')
+            break
+        else:
+            idx += 1
+    if idx == extLen:
+        print(basename)
+print(len(data))
+random.shuffle(data)
+trainFile = open("train.txt", "wt")
+validFile = open("valid.txt", "wt")
+for line in data:
+    trainFile.write(line)
+    validFile.write(line)
+trainFile.close()
+validFile.close()

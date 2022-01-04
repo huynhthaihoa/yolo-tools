bb76e26664fd0106e8d7b2c75a610366029144af
Hoa <huynhlequang@gmail.com>
2022-01-04 14:51:59 +0900

    add move_out_range_anno.py: pick annotation file which has the number of bounding boxes outside of the specified range (01/04/2022 - 14:51)

---
+++ move_out_range_anno.py
@@ -0,0 +1,39 @@
+import os
+from glob import glob
+import argparse
+import numpy as np
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--input", help="Directory of input folder",
+                    type=str, default="img")
+    parser.add_argument("-o", "--output", help="Directory of output folder",
+                    type=str, default="out")
+    parser.add_argument("-u", "--maximum", help="Maximum number of bounding boxes in one image",
+                    type=int, default=100)
+    parser.add_argument("-l", "--minimum", help="Minimum number of bounding boxes in one image",
+                    type=int, default=1)
+    args = parser.parse_args()
+    inpDir = args.input + '/'
+    outDir = args.output + '/'
+    
+    if os.path.isdir(outDir) is False:
+        os.mkdir(outDir)
+
+    anns = glob(inpDir + '*.txt')
+    
+    for ann in anns:
+        basename = os.path.basename(ann)
+        newann = outDir + basename
+        name, _ = os.path.splitext(basename)
+        img = inpDir + name + ".png"
+        newimg = outDir + name + ".png"
+        if os.path.exists(img) is True:
+            count = len(np.loadtxt(ann).reshape(-1, 5))
+            if count > args.maximum or count < args.minimum:
+                os.rename(ann, newann)
+                os.rename(img, newimg)
+            print(".", end="", flush=True)
+            
+    print("Completed!")
+        

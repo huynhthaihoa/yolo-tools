7fb9aa64c350bf1a0e11b3de6758275cc107bfee
Hoa <huynhlequang@gmail.com>
2022-01-21 11:06:39 +0900

    append_annotation.py: fix bug on updating origin file (01/21/2022 - 11:06)

--- append_annotation.py
+++ append_annotation.py
@@ -12,7 +12,7 @@ with open(origin, 'r') as file:
 #data = np.loadtxt(origin)#.reshape(-1, 5)
 for filename in filenames:
     print(filename)
-    if filename == origin:
+    if filename == os.path.basename(origin):
         continue
     anno = open(filename, "at")
     anno.write(data)

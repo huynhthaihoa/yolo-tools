ab6d157dbebeb69f8d9903506c88c6295b7d6d7b
Hoa <huynhlequang@gmail.com>
2022-02-15 10:18:36 +0900

    augment_images.py: add functions countDigit & getSuffix; set default dir of inputImgs & outputImgs as empty; remove random snow; add randomsizebboxsafecrop & perspective (02/15/2022 - 10:18)

--- augment_images.py
+++ augment_images.py
@@ -7,6 +7,36 @@ import os
 from glob import glob
 #from albumentations.augmentations.transforms import Posterize
 
+def countDigit(number):
+    '''
+    count the number of digits of a specified number:
+    @number [in]: a number to be determined
+    '''
+    count = 0
+    while(number > 0):
+        count += 1
+        number = number // 10
+    return count
+
+def getSuffix(nDigits, index):
+    '''
+    generate image file name suffix:
+    @nDigits [in]: # of digit to represent all the frames,
+    @index [in]: index of the selected image
+    '''
+    testNum = 10
+    count = 1
+    while(testNum <= index):
+        count += 1
+        testNum *= 10
+    suffix = ""
+    count = nDigits - count
+    #print(count)
+    for i in range(count):
+        suffix += "0"
+    suffix += str(index)
+    return suffix
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
@@ -15,20 +45,25 @@ if __name__ == "__main__":
     parser.add_argument("-oa", "--output_ann", help="Directory of augmented annotation files",
                     type=str, default="coco/yolo/augmented")
     parser.add_argument("-ii", "--input_img", help="Directory of original images",
-                    type=str, default="coco/images")
+                    type=str, default="")
     parser.add_argument("-oi", "--output_img", help="Directory of augmented images",
-                    type=str, default="coco/images/augmented")
+                    type=str, default="")
     parser.add_argument("-n", "--number", help="Number of augmented images for each original images (default is 10)",
                     type=int, default=10)
     args = parser.parse_args()
     inputAnns = args.input_ann + '/'
     outputAnns = args.output_ann
     n = args.number
+    nDigits = countDigit(n)
     if os.path.isdir(outputAnns) is False:
         os.mkdir(outputAnns)
     outputAnns += '/'
     inputImgs = args.input_img
+    if inputImgs == "":
+        inputImgs = inputAnns
     outputImgs = args.output_img
+    if outputImgs == "":
+        outputImgs = outputAnns
     if os.path.isdir(outputImgs) is False:
         os.mkdir(outputImgs)
     outputImgs += '/'
@@ -48,6 +83,7 @@ if __name__ == "__main__":
                 bboxes.append([float(elems[1]), float(elems[2]), float(elems[3]), float(elems[4]), int(elems[0])])
             lines.close()
             image = cv2.imread(imgpath)
+            height, width, _ = image.shape
             for i in range(n):#A.ShiftScaleRotate(p=0.2)
                 print(".", end="", flush=True)
                 transform_list = list()
@@ -55,18 +91,28 @@ if __name__ == "__main__":
                 seed_weather = random.randint(0, 2)
                 seed_contrast = random.randint(0, 2)
                 seed_rotate = random.randint(0, 2)
-                if seed_weather == 0:
-                    transform_list.append(A.RandomSnow(p=0.5))
-                else:
+                # seed_transpose = random.randint(0, 2)
+                seed_safecrop = random.randint(0, 2)
+                seed_perspective = random.randint(0, 2)
+                # if seed_weather == 0:
+                #     transform_list.append(A.RandomSnow(p=0.5))
+                # else:
+                if seed_weather == 1:
                     transform_list.append(A.RandomRain(p=0.5))
                 if seed_contrast == 1:
                     transform_list.append(A.RandomBrightnessContrast(p=0.5))
                 if seed_rotate == 1:
                     transform_list.append(A.ShiftScaleRotate(p=0.5, rotate_limit=15))
+                # if seed_transpose == 1:
+                #     transform_list.append(A.Transpose(p=0.5))
+                if seed_safecrop == 1:
+                    transform_list.append(A.RandomSizedBBoxSafeCrop(height, width))
+                if seed_perspective:
+                    transform_list.append(A.Perspective())
                 transform = A.Compose(transforms=transform_list, bbox_params=A.BboxParams(format="yolo"))
                 res = transform(image=image, bboxes=bboxes)
-                outputimgpath = outputImgs + originname + '-' + str(i) + '.jpg'
-                outputannpath = outputAnns + originname + '-' + str(i) + '.txt'
+                outputimgpath = outputImgs + originname + '-' + getSuffix(nDigits, i) + '.png'
+                outputannpath = outputAnns + originname + '-' + getSuffix(nDigits, i) + '.txt'
                 cv2.imwrite(outputimgpath, res["image"])
                 outfile = open(outputannpath, "wt")
                 for elems in res["bboxes"]:

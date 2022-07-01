f96f03f97be1943901a03ef8c3fa9127ed100fc0
Hoa <huynhlequang@gmail.com>
2022-07-01 09:50:58 +0900

    remove_wrong_detection.py: move error image into other folder (07/01/2022)

---
+++ remove_wrong_detection.py
@@ -0,0 +1,29 @@
+import argparse
+import os 
+from glob import glob
+
+if __name__ == "__main__":
+    
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("-i", "--input", help="Path to the input folder",
+                    type=str, default="input")
+    parser.add_argument("-r", "--reference", help="Path to the reference folder",
+                    type=str, default="refence")
+    parser.add_argument("-o", "--output", help="Path to the output folder", 
+                    type=str, default="output")
+    
+    args = parser.parse_args()
+    
+    rfnames = glob(os.path.join(args.reference, '*.jpg'))
+    rbnames = [os.path.basename(fname) for fname in rfnames]
+    
+    fnames = glob(os.path.join(args.input, '*.jpg'))
+    for fname in fnames:
+        bname = os.path.basename(fname)
+        if bname not in rbnames:
+            print(bname)
+            os.rename(fname, os.path.join(args.output, bname))
+    print("Finish!")
+    
+    

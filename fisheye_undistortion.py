0ac199148a6b2b6062287355b343af0f7465462c
Hoa <huynhlequang@gmail.com>
2022-07-07 10:28:41 +0900

    fisheye_undistortion.py: support 2 calibration file type (dat/json), add scaling for general cases (07/07/2022)

--- fisheye_undistortion.py
+++ fisheye_undistortion.py
@@ -6,16 +6,17 @@ Fisheye distortion based on Scaramuzza Ocamcalib toolbox:
 import cv2
 import numpy as np
 import argparse
-import pickle
+import json
 import os
 from glob import glob
+import pickle
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-i", "--input", help="Path to the input folder",
                     type=str, default="")
-    parser.add_argument("-c", "--calibration", help="Path to the output calibration file",
-                    type=str, default="calibration.dat")
+    parser.add_argument("-c", "--calibration", help="Path to the calibration file (json/dat)",
+                    type=str, default="calibration.json")
     parser.add_argument("-o", "--output", help="Path to the output folder",
                     type=str, default="")
     parser.add_argument("-b", "--balance", help="Balance value",
@@ -25,20 +26,34 @@ if __name__ == "__main__":
     
     INPUT_DIR = args.input
     
-    with open(args.calibration, "rb") as f:
-        data = pickle.load(f)
-        dim, ret, K, D, rvecs, tvecs = data
-        print(dim)
-        print(K)
-        print(D)
+    calibration = args.calibration
+    ext = calibration.split('.')[-1]
+    if ext == 'dat':
+        with open(args.calibration, "rb") as f:
+            data = pickle.load(f)
+        # data = json.load(f)
+            dim, ret, K, D, rvecs, tvecs = data
+    elif ext == 'json':
+        with open(args.calibration, "r") as f:
+            data = json.load(f)
+            dim = data['dim']
+            ret = data['rms']
+            K = np.array(data['K'])
+            D = np.array(data['D'])
+            rvecs = data['rvecs']
+            tvecs = data['tvecs']
+        #dim, ret, K, D = data
+    print(dim)
+    print(K)
+    print(D)
         #newcameramtx, roi=cv2.getOptimalNewCameraMatrix(mtx, dist, dim, 1)
         # undistort
         #mapx, mapy = cv2.initUndistortRectifyMap(mtx, dist, None, newcameramtx, dim, 5)
         #x, y, w, h = roi
-        newcameramtx = cv2.fisheye.estimateNewCameraMatrixForUndistortRectify(K, D, dim, np.eye(3), balance=args.balance)
-        mapx, mapy = cv2.fisheye.initUndistortRectifyMap(K, D, np.eye(3), newcameramtx, dim, cv2.CV_16SC2)
-        print(mapx)
-        print(mapy)
+        # newcameramtx = cv2.fisheye.estimateNewCameraMatrixForUndistortRectify(K, D, dim, np.eye(3), balance=args.balance)
+        # mapx, mapy = cv2.fisheye.initUndistortRectifyMap(K, D, np.eye(3), newcameramtx, dim, cv2.CV_16SC2)
+        # print(mapx)
+        # print(mapy)
         
     OUTPUT_DIR = args.output
     if os.path.isdir(OUTPUT_DIR) is False:
@@ -50,6 +65,17 @@ if __name__ == "__main__":
         print(fname)
         basename = os.path.basename(fname)
         img = cv2.imread(fname)
+        img_dim = img.shape[:2][::-1]
+        
+        scaled_K = K * img_dim[0] / dim[0]
+        scaled_K[2][2] = 1.0
+        
+        newcameramtx = cv2.fisheye.estimateNewCameraMatrixForUndistortRectify(
+            scaled_K, D, img_dim, np.eye(3), balance = args.balance
+        )
+        mapx, mapy = cv2.fisheye.initUndistortRectifyMap(
+            scaled_K, D, np.eye(3), newcameramtx, img_dim, cv2.CV_16SC2
+        )
         
         #undistort
         #dst = cv2.fisheye.undistortImage(img, mtx, dist, None, newcameramtx)
